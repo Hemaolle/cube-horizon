d3ebd0b8a386b3073039ad4be1cd368019f0a23d
Oskari Leppaaho <oskari.leppaaho@gmail.com>
2013-02-23 16:23:42 +0200

    Added forwardDirection to global variables.

--- Assets/Scripts/GlobalVariables.js
+++ Assets/Scripts/GlobalVariables.js
@@ -1,12 +1,19 @@
 #pragma strict
 /**
- * A static class for keeping track of world gravity.
+ * A static class for keeping track of global variables.
  * 
  * Authors: Mikko Jakonen, Oskari Leppäaho
- * Version 0.2
+ * Version 0.3
  */
-static var gravity : Vector3 = Vector3.down * 9.81;
+ 
+//Gravity strength.
+private static var gravityMultiplier = 50;
+static var gravity : Vector3 = Vector3.down * gravityMultiplier;
+static var forwardDirection : Vector3 = Vector3.right;
 
-static function change(transform:Transform) {
-	gravity = transform.TransformDirection(Vector3.down * 9.81);
+static function changeGravityDirection(transform:Transform) {
+	gravity = transform.TransformDirection(Vector3.down * gravityMultiplier);
+	forwardDirection = transform.TransformDirection(Vector3.right);
+	forwardDirection.Normalize();
 }
+
